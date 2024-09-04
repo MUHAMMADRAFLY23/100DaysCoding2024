188f7376423910bcee9bbd86a1e5093257b8c5a5
MUHAMMADRAFLY23 <aaffi1545@gmail.com>
2024-09-04 12:15:29 +0800

    Hari3

---
+++ Day 3.java
@@ -0,0 +1,19 @@
+package day3;
+
+
+public class Day3 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("rafly");
+        for (int i = 0; i < 100; i++) {
+            int hasil= i%2;
+            if (hasil==0) {
+                System.out.println(i);
+            }
+            
+        }
+     
+    }
+    
+}
