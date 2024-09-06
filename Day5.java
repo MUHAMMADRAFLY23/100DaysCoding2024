482fa7efb8c6f287ec98a541e63ae73a29b4f28c
MUHAMMADRAFLY23 <aaffi1545@gmail.com>
2024-09-06 20:42:21 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,11 @@
+class Day5
+ {
+   public static void main(String args[])
+   { 
+    //System.out.println("Hello world");
+    /* 
+      ada dua tipe komentar yang pertama ada tanda // dan kedua /* ini dimaksudkan untuk teks yang banyak atau menutupi eror sementara yang ada di program
+     */
+      System.out.println("Day5")
+   }
+ }
