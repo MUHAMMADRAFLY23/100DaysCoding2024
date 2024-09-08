da4babc305a3486bbe69466345cc6a3215685224
MUHAMMADRAFLY23 <aaffi1545@gmail.com>
2024-09-08 18:43:40 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,19 @@
+public class day7 {
+    public static void main(String[] args) {
+       //Pemberian Nilai Dalam Variabel
+       String a ="Muhammad Rafly";
+       int b = 19;
+       double c = 1.60;
+       float d = 45.f;
+       char e = 'L';
+       boolean f = true;
+        System.out.println("Nama \t\t: "+a);
+        System.out.println("Umur \t\t: "+b+"th");
+        System.out.println("Tinggi \t\t: "+c+" m");
+        System.out.println("Berat \t\t: "+d+" kg");
+        System.out.println("Gender (P/L)\t: "+e);
+        
+  
+    
+}
+}
