0529d4673473c4ca6da4a44014487bd23ccae571
MUHAMMADRAFLY23 <aaffi1545@gmail.com>
2024-09-09 19:21:22 +0800

    Day8_variabelUpdate.java

---
+++ Day8.java
@@ -0,0 +1,12 @@
+
+public class raff8day { 
+    public static void main(String [] args){
+        int n = 15;
+        System.out.println(n);
+        n = 7;
+        System.out.println("variabel setelah di update: "+n);
+        
+        
+    }
+    
+}
