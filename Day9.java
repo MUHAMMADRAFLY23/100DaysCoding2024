e83c06ddd58905e885a82a17e802e43a5e3c1ed6
MUHAMMADRAFLY23 <aaffi1545@gmail.com>
2024-09-10 21:38:12 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,10 @@
+public class day9 {
+    public static void main(String[] args) {
+        //Variabel final
+        final int angka =100;
+        /* angka =200; akan menyebabkan eror 
+           karena nilai angka tidak dapat diubah
+        */
+        System.out.println("Nilai Maksimum : "+angka);
+    }
+}
